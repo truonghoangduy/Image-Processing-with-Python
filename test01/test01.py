dc43e06a1a6dd5988821d5ff9cdef411e3829c3a
truonghoangduy <truonghoangduy03111999@gmail.com>
2020-05-28 22:54:01 +0700

    adding desription & fix from colorsimg to grayscale img on prewiit

--- test01/test01.py
+++ test01/test01.py
@@ -5,8 +5,12 @@ from matplotlib import pyplot as plt
 # import skimage
 from skimage import filters
 
+
 # Load Image
 image = cv2.imread('picture/05.jpg')
+# Load Result in full HD
+resultImage = cv2.imread("result.png")
+
 #Prewiit Kernal  
 # // Could be use on cv2.filter2D :
 #  Aka 2 kernal 3x3 kernal
@@ -19,9 +23,14 @@ kernalx5 = np.array([
 [-1,-1,-2,-1,-1],
 [-2,-2,-4,-2,-2]])
 kernaly5 = np.array([[2,1,0,-1,-2],[2,1,0,-1,-2],[4,2,0,-2,-4],[2,1,0,-1,-2],[2,1,0,-1,-2]])
-
+#-------
+def showResult():
+     cv2.namedWindow("Result",cv2.WINDOW_NORMAL)
+     cv2.resizeWindow("Result", 1000,1000)
+     cv2.imshow("Result",resultImage)
 def showImage(name,image,x,y):
      # show image with opencv
+     # x | y for your window location
      cv2.namedWindow(name,cv2.WINDOW_NORMAL)
      cv2.resizeWindow(name, 400,400)
      cv2.moveWindow(name,x,y)
@@ -29,7 +38,7 @@ def showImage(name,image,x,y):
 
 def rbgHistogram(image):
      color = ('b','g','r')
-     for i,col in enumerate(color):
+     for i,col in enumerate(color): # Going through each RBG 0-255 :)) happy loop
           histr = cv2.calcHist([image],[i],None,[256],[0,256])
           plt.plot(histr,color = col)
           plt.xlim([0,256])
@@ -37,20 +46,30 @@ def rbgHistogram(image):
 def prewittKernal(name,image,kernelx,kernely,x,y):
      imageX = cv2.filter2D(image,-1,kernelx)
      imageY =cv2.filter2D(image,-1,kernely)
-     # Origanal to megre two image after filter 
+     # Origanal to megre two image after filter with Atan2
      # atan22 = cv2.fastAtan2(imageX.flatten(),imageY.flatten())
      # prewittFlitter= cv2.addWeighted(imageX,0.5,imageY,0.5,0)
+
+     # Megring 2 differ image after filter by using cv2.add or cv2.addWeighted
+     # Result is one picture hold the combie of all
      prewittFlitter= cv2.add(imageX,imageY)
      showImage(name,prewittFlitter,x,y)     
 
+
 # Gray scale
 grayImage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-#Blur Prewitt with skiImage
-blurPrewitt = filters.prewitt(image)
+
+#Blur Prewitt with skiImage blur on grayscale
+# blurPrewitt = filters.prewitt(image)
+
+blurPrewitt = filters.prewitt(grayImage)
+
 # BW image thresh hold 127 from gray scle imgae
 (thresh,bWimage) = cv2.threshold(grayImage,127,255,cv2.THRESH_BINARY)
 print(thresh)
+showResult()
 # Call function For show Image
+
 showImage("Orgianal",image,0,0)
 showImage("Prewitt Blur",blurPrewitt,400,0)
 showImage("GrayImage",grayImage,800,0)
@@ -59,15 +78,17 @@ rbgHistogram(image)
 
 # Call function For Prewwit DIY kernal show Image
 
-prewittKernal("Prewitt3x3",grayImage,kernelx3,kernely3,1400,400)
-prewittKernal("Prewitt5x5",grayImage,kernalx5,kernaly5,1700,400)
-plt.hist(grayImage.ravel(),256,[0,255],label="Gray Color Distibution")
+prewittKernal("Prewitt3x3",grayImage,kernelx3,kernely3,800,500)
+prewittKernal("Prewitt5x5",grayImage,kernalx5,kernaly5,900,500)
+
+# plt.hist(grayImage.ravel(),256,[0,255],label="Gray Color Distibution")
 plt.show()
 
 
 
 
-
+# View on PC turn of the plot frist and then hit enter on any
+# Picture pop op will close python script
 key = cv2.waitKey(0)
 if key == 13:
      plt.close()
